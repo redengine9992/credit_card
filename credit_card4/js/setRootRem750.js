380ba55b0ff58ef9d924b1110a11e52cfc60cdcb
redengine9992 <44667126+redengine9992@users.noreply.github.com>
2019-06-23 12:02:37 +0800

    Add files via upload

---
+++ credit_card4/js/setRootRem750.js
@@ -0,0 +1,1 @@
+setFontSize();window.addEventListener("resize",setFontSize,false);function setFontSize(){var clientW=document.documentElement.clientWidth;if(clientW>750){clientW=750}document.documentElement.style.fontSize=clientW/7.5+"px"};
